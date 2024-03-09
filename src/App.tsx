431788b491ac84bc795cd0b1ea91d1273675b288
Isaac Day <dayisaacm81@gmail.com>
2024-03-08 17:32:00 -0700

    basics finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
+import teamData from './CollegeBasketballTeams.json';
 import './App.css';
+
+const teamList = teamData.teams;
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <h3>
+      In honor of the upcoming March Madness Tournament, here is a list of all
+      basketball teams in the NCAA. You're welcome.
+    </h3>
+  );
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>
+          School: {oneTeam.school} {oneTeam.name}
+        </h2>
+        <h3>City: {oneTeam.city}</h3>
+        <h3>State: {oneTeam.state}</h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamList.map((singleTeam) => (
+        <Team {...singleTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
